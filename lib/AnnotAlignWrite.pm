25217073dd09489eb0885e1f60b2fe0e649dedac
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-04-03 13:14:39 +0000

    printing aligned sentences to annotate also for a-addresses
    Wed Mar  5 13:36:49 CET 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@12273 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/AnnotAlignWrite.pm
+++ lib/AnnotAlignWrite.pm
@@ -41,6 +41,14 @@ sub _linearize_tnode {
     if (any {$_ == $tnode} @highlight) {
         $word = "<" . $word . ">";
     }
+    my @hl_anodes = grep {$_->get_layer eq "a"} @highlight;
+    my ($hl_anode_idx) = grep {
+        my ($hl_tnode) = $hl_anodes[$_]->get_referencing_nodes('a/aux.rf');
+        defined $hl_tnode && $hl_tnode == $tnode
+    } 0 .. $#hl_anodes;
+    if (defined $hl_anode_idx) {
+        $word = "<__A:". $hl_anodes[$hl_anode_idx]->id ."__". $word . ">";
+    }
     return $word;
 }
 
@@ -53,26 +61,41 @@ sub _linearize_ttree {
     return join " ", @words;
 }
 
-sub process_tnode {
-    my ($self, $l1_tnode) = @_;
+sub _process_node {
+    my ($self, $l1_node) = @_;
 
     log_fatal "Must be run on 'ref'."
         if ($self->selector ne "ref");
 
-    log_info $l1_tnode->id;
-    my ($l2_tnode) = Treex::Tool::Align::Utils::aligned_transitively([$l1_tnode], [$self->robust_align_filter]);
+    log_info $l1_node->id;
+    my ($l2_node) = Treex::Tool::Align::Utils::aligned_transitively([$l1_node], [$self->robust_align_filter]);
+    if (!defined $l2_node && $l1_node->get_layer eq "t") {
+        my $l1_anode = $l1_node->get_lex_anode;
+        if (defined $l1_anode) {
+            ($l2_node) = Treex::Tool::Align::Utils::aligned_transitively([$l1_anode], [$self->robust_align_filter]);
+        }
+    }
 
-    my $l1_zone = $l1_tnode->get_zone;
-    my $l2_zone = $l1_tnode->get_bundle->get_zone($self->align_lang, "ref");
+    my $l1_zone = $l1_node->get_zone;
+    my $l2_zone = $l1_node->get_bundle->get_zone($self->align_lang, "ref");
 
-    print {$self->_file_handle} $l1_tnode->get_address . "\n";
+    print {$self->_file_handle} $l1_node->get_address . "\n";
     print {$self->_file_handle} $l1_zone->sentence . "\n";
     print {$self->_file_handle} $l2_zone->sentence . "\n";
-    print {$self->_file_handle} _linearize_ttree($l1_zone->get_ttree, $l1_tnode) . "\n";
-    print {$self->_file_handle} _linearize_ttree($l2_zone->get_ttree, $l2_tnode) . "\n";
+    print {$self->_file_handle} _linearize_ttree($l1_zone->get_ttree, $l1_node) . "\n";
+    print {$self->_file_handle} _linearize_ttree($l2_zone->get_ttree, $l2_node) . "\n";
     print {$self->_file_handle} "ERR:\n";
     print {$self->_file_handle} "\n";
 }
 
+sub process_tnode {
+    my ($self, $l1_tnode) = @_;
+    $self->_process_node($l1_tnode);
+}
+
+sub process_anode {
+    my ($self, $l1_anode) = @_;
+    $self->_process_node($l1_anode);
+}
 
 1;
