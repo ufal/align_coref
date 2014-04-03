d57b7a72dc7aab7002c195c8bf3dc9189fd18f14
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-04-03 13:14:14 +0000

    printer for align data tables
    Tue Mar  4 18:42:59 CET 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@12266 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/PrintAlignData.pm
@@ -0,0 +1,71 @@
+package Treex::Block::My::PrintAlignData;
+
+use Moose;
+use Treex::Core::Common;
+
+use Treex::Tool::ML::TabSpace::Util;
+
+use Treex::Tool::Align::Utils;
+
+use Treex::Tool::Align::FeaturesRole;
+use Treex::Tool::Align::Features;
+
+extends 'Treex::Block::Write::BaseTextWriter';
+
+has 'align_language' => (is => 'ro', isa => 'Str', required => 1);
+has 'gold_align_filter' => (is => 'ro', isa => 'HashRef', builder => '_build_gaf');
+
+has '_feat_extractor' => (is => 'ro', isa => 'Treex::Tool::Align::FeaturesRole', builder => '_build_feat_extractor');
+
+sub _build_feat_extractor {
+    my ($self) = @_;
+    return Treex::Tool::Align::Features->new();
+}
+
+sub BUILD {
+    my ($self) = @_;
+    $self->gold_align_filter;
+}
+
+sub _build_gaf {
+    my ($self) = @_;
+    return { language => $self->align_language, rel_types => ['gold','.*'] };
+}
+
+sub _get_candidates {
+    my ($self, $tnode) = @_;
+    my $aligned_ttree = $tnode->get_bundle->get_zone($self->align_language, $self->selector)->get_ttree();
+    my @candidates = $aligned_ttree->get_descendants({ordered => 1});
+    return @candidates;
+}
+
+sub _get_positive_candidate {
+    my ($self, $tnode) = @_;
+
+    # TODO: My::ProjectAlignment has to be called upfront
+    # better to put it here
+
+    return Treex::Tool::Align::Utils::aligned_transitively([$tnode], [$self->gold_align_filter]);
+}
+
+sub _get_positive_cand_idx {
+    my ($cands, $pos_cand) = @_;
+
+    my ($pos_idx) = grep {$_ == $pos_cand} 0 .. $#$cands;
+    return $pos_idx;
+}
+
+sub process_tnode {
+    my ($self, $tnode) = @_;
+
+    my @cands = $self->_get_candidates($tnode);
+    my $feats = $self->_feat_extractor->create_instances($tnode, \@cands);
+    
+    my ($gold_aligned_node) = $self->_get_positive_candidate($tnode);
+    my $pos_cand_idx = _get_positive_cand_idx(\@cands, $gold_aligned_node);
+
+    my $instance_str = Treex::Tool::ML::TabSpace::Util::format_multiline($feats, [$pos_cand_idx]);
+    print {$self->_file_handle} $instance_str;
+}
+
+1;
