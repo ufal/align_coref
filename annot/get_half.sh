ba43cb387017aad120583ff66db5e5b4969824c0
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:13:36 +0000

    merging annotation, so far just from 1900 to 1949
    Tue Apr 22 11:45:07 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13298 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ annot/get_half.sh
@@ -0,0 +1,4 @@
+cat en_perspron/align.ref.sec19.all.ali_annot | head -n 4046 > en.all.align.ref.sec19_00-49.ali_annot
+cat en_perspron_unexpr/align.ref.sec19.all.ali_annot | head -n 700 >> en.all.align.ref.sec19_00-49.ali_annot
+cat en_relpron/align.ref.sec19.anja.ali_annot | head -n 371 >> en.all.align.ref.sec19_00-49.ali_annot
+cat en_cor/align.ref.sec19.structured.all.pt1.ali_annot >> en.all.align.ref.sec19_00-49.ali_annot
