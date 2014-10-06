520c11f39f5bf50fd13d54bff818029381d65155
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:14:16 +0000

    statistics on selected covering the coreferential
    Wed Apr 23 12:25:40 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@13316 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ annot/scripts/coverage.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# ./coverage.sh <coref_file> <align_annot_file> <expr_type>
+coref_file=$1
+align_annot_file=$2
+expr_type=$3
+
+
+function perc {
+    perl -e 'my ($a, $b) = @ARGV; printf "%.2f%%\n", $a / $b * 100;' $1 $2
+}
+
+tmp_dir=/COMP.TMP/coref_align_coverage
+mkdir $tmp_dir
+
+cat $coref_file | cut -f1,5 | sed 's/\/.*\///g' | sed 's/treex\.gz/streex/' | sort -k2,2 > $tmp_dir/coref.ids
+cat $align_annot_file |  sed -n '1~7p' | sed 's/^.*\///g' | sort > $tmp_dir/sel.ids
+join -1 2 $tmp_dir/coref.ids $tmp_dir/sel.ids > $tmp_dir/both.ids
+
+sel_count=`cat $tmp_dir/sel.ids | wc -l`
+coref_count=`cat $tmp_dir/coref.ids | wc -l`
+type_count=`cat $tmp_dir/coref.ids | grep "^$expr_type	" | wc -l`
+both_count=`cat $tmp_dir/both.ids | wc -l`
+
+echo -n "# Selected: "
+echo $sel_count
+echo -n "# Coreferential: "
+echo $coref_count
+echo -n "# Coreferential of \"$expr_type\" type: "
+echo $type_count
+echo -n "# Coreferential and selected: "
+echo $both_count
+
+echo -n "Coreferential out of selected: "
+perc $both_count $sel_count
+echo -n "Coverage of all coreferential: "
+perc $both_count $coref_count
+echo -n "Coverage of coreferential of \"$expr_type\" type: "
+perc $both_count $type_count
+
+#rm -rf $tmp_dir
