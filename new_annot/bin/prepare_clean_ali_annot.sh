5f57b0af7e175a02c74faa4b3bbab00097a24981
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-02-12 14:02:49 +0100

    running on cluster

--- new_annot/bin/prepare_clean_ali_annot.sh
+++ new_annot/bin/prepare_clean_ali_annot.sh
@@ -7,6 +7,6 @@ layers=$4
 node_types=$5
 align_langs=$6
 
-treex -L$language -S$selector \
+treex -p --jobs=50 -L$language -S$selector \
     Read::Treex from=@$src_tree_list \
 	Align::Annot::Print layers=$layers align_langs=$align_langs node_types=$node_types
