b32e97aa6e4ddf89e79bc93f974ac30cd96da673
Michal Novak <mnovak@ufal.mff.cuni.cz>
2017-08-22 11:46:21 +0200

    skip_finished support for readers + lemmatized bitexts to part files first, then merge

--- AnaphBus/align4treex/scripts/analyse_to_treex.sh
+++ AnaphBus/align4treex/scripts/analyse_to_treex.sh
@@ -14,16 +14,22 @@ parse_lpair $lpair
 
 mkdir -p $outdir
 
+indir=${inpath#!}
+indir=`dirname "$indir"`
+inre="$indir/(.*)\.txt$"
+outre="$outdir/"'$1.treex.gz'
+
 reader="Read::SentencesTSV from=$inpath langs=$lpaircomma"
 if [[ "$inpath" == *.treex.gz ]]; then
     reader="Read::Treex from=$inpath"
+    inre="$indir/(.*)\.treex.gz$"
 fi
 
 l1_scen=`$my_dir/../scenario/$l1.lemmatize.sh $tokenize`
 l2_scen=`$my_dir/../scenario/$l2.lemmatize.sh $tokenize`
 
 run_treex -Salign \
-    $reader \
+    $reader skip_finished="{$inre}{$outre}" \
     $l1_scen \
     $l2_scen \
     Write::Treex path=$outdir
--- AnaphBus/align4treex/scripts/print_lemmatized_bitext.sh
+++ AnaphBus/align4treex/scripts/print_lemmatized_bitext.sh
@@ -13,8 +13,14 @@ max_tokens=${4:-0}
 parse_lpair $lpair
 
 mkdir -p $(dirname $outfile)
+mkdir -p $RUN_DIR/for_giza.files
+
+indir=${inpath#!}
+indir=`dirname "$indir"`
+inre="$indir/(.*)\.treex.gz$"
+outre="$RUN_DIR/for_giza.files/"'$1.txt'
 
 run_treex \
-    Read::Treex from=$inpath \
-    Write::LemmatizedBitexts selector=align language=$l1 to_language=$l2 to_selector=align max_tokens=$max_tokens | \
-gzip -c > $outfile
+    Read::Treex from=$inpath skip_finished="{$inre}{$outre}" \
+    Write::LemmatizedBitexts path=$RUN_DIR/for_giza.files extension=.txt selector=align language=$l1 to_language=$l2 to_selector=align max_tokens=$max_tokens
+find $RUN_DIR/for_giza.files -name '*.txt' | xargs cat | gzip -c > $outfile
