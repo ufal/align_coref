9486d7dc7f6844531530312df3c7312a5f30bb90
Michal Novak <mnovak@ufal.mff.cuni.cz>
2018-01-16 00:27:55 +0100

    counterpart statistics on CzEng
    - summary printer moved to PAWS (AnaphBus) lib directory and adjusted
    - makefile to collect the statistics
    - readme file explaining so far collected statistics

--- AnaphBus/multi/lib/Treex/Block/AnaphBus/CrossLingStats.pm
+++ AnaphBus/multi/lib/Treex/Block/AnaphBus/CrossLingStats.pm
@@ -1,37 +1,63 @@
-package Treex::Block::My::BitextCorefSummary;
+package Treex::Block::AnaphBus::CrossLingStats;
 
 use Moose;
 use Treex::Core::Common;
 
 use Treex::Tool::Align::Utils;
-use Treex::Tool::Coreference::NodeFilter::PersPron;
-use Treex::Block::My::CorefExprAddresses;
+use Treex::Tool::Coreference::NodeFilter;
+
+use Data::Printer;
 
 extends 'Treex::Block::Write::BaseTextWriter';
+with 'Treex::Block::Filter::Node';
 
 has 'align_lang' => (is => 'ro', isa => 'Str', required => 1);
 
-has 'gold_align_filter' => (is => 'ro', isa => 'HashRef', builder => '_build_gaf');
+has 'gold_align_filter' => (is => 'ro', isa => 'HashRef', lazy => 1, builder => '_build_gaf');
+has 'align_type' => ( is => 'ro', isa => 'Str', default => 'gold' );
+has 'counter_types' => ( is => 'ro', isa => 'ArrayRef[Str]', builder => '_build_counter_types', lazy => 1 );
 
 sub BUILD {
     my ($self) = @_;
+    $self->counter_types;
     $self->gold_align_filter;
+}
+
+sub _build_node_types {
+    my ($self) = @_;
+    return [
+        'perspron', 
+        'perspron_unexpr', 
+        'relpron',
+        'cor',
+    ];
+}
+
+sub _build_counter_types {
+    my ($self) = @_;
+    my @types = @{$self->node_types};
+    push @types, (
+        '#perspron.12.no_refl',
+        'demonpron',
+        'noun.only',
+    );
+    return \@types;
 }
 
 sub _build_gaf {
     my ($self) = @_;
-    return { language => $self->align_lang, rel_types => ['gold'] };
+    return { language => $self->align_lang, selector => $self->selector, rel_types => [$self->align_type] };
 }
 
 sub feats_for_tnode {
-    my ($tnode) = @_;
+    my ($self, $tnode) = @_;
 
     return map {"undef"} 1..7 if (!defined $tnode);
 
     my @feats = ();
 
     push @feats, $tnode->get_address;
-    push @feats, get_type($tnode);
+    push @feats, $self->get_type($tnode);
     push @feats, $tnode->t_lemma // "undef";
     push @feats, $tnode->gram_sempos // "undef";
 
@@ -54,13 +80,13 @@ sub feats_for_tnode {
 }
 
 sub feats_for_anode {
-    my ($anode) = @_;
+    my ($self, $anode) = @_;
     
     return map {"undef"} 1..7 if (!defined $anode);
     
     my @feats = ();
     push @feats, $anode->get_address;
-    push @feats, get_type($anode);
+    push @feats, $self->get_type($anode);
     push @feats, map {"undef"} 1..2;
     push @feats, $anode->lemma;
     push @feats, substr($anode->tag, 0, 2);
@@ -76,47 +102,42 @@ sub get_ali_info {
 }
 
 sub get_type {
-    my ($node) = @_;
-    my $type = "undef";
-    if (Treex::Tool::Coreference::NodeFilter::PersPron::is_3rd_pers($node, {expressed => 1})) {
-        $type = "perspron";
+    my ($self, $node) = @_;
+    my $node_types;
+    if ($node->language eq $self->language) {
+        $node_types = $self->node_types;
     }
-    elsif (Treex::Tool::Coreference::NodeFilter::PersPron::is_3rd_pers($node, {expressed => -1})) {
-        $type = "perspron_unexpr";
+    else {
+        $node_types = $self->counter_types;
     }
-    elsif (Treex::Tool::Coreference::NodeFilter::RelPron::is_relat($node)) {
-        $type = "relpron";
+    my @matched_types = Treex::Tool::Coreference::NodeFilter::get_matched_set($node, $node_types);
+    if (!@matched_types) {
+        @matched_types = ( "undef" );
     }
-    elsif (Treex::Block::My::CorefExprAddresses::_is_cor($node)) {
-        $type = "cor";
-    }
-    elsif (Treex::Block::My::CorefExprAddresses::_is_cs_ten($node)) {
-        $type = "ten";
-    }
-    $type = $node->language . "_" . $type;
+    my $type = $node->language . "_" . join(";", sort @matched_types);
     return $type;
 }
 
-sub process_tnode {
+sub process_filtered_tnode {
     my ($self, $l1_tnode) = @_;
 
-    my @l1_feats = feats_for_tnode($l1_tnode);
+    my @l1_feats = $self->feats_for_tnode($l1_tnode);
     my ($l2_tnode) = Treex::Tool::Align::Utils::aligned_transitively([$l1_tnode], [$self->gold_align_filter]);
     my @l2_feats;
     my $l1_anode = $l1_tnode->get_lex_anode;
     my $ali_info = undef;
     if (!defined $l2_tnode && defined $l1_anode) {
         my ($l2_anode) = Treex::Tool::Align::Utils::aligned_transitively([$l1_anode], [$self->gold_align_filter]);
-        @l2_feats = feats_for_anode($l2_anode);
+        @l2_feats = $self->feats_for_anode($l2_anode);
         $ali_info = get_ali_info($l1_anode, $l2_anode);
     }
     else {
-        @l2_feats = feats_for_tnode($l2_tnode);
+        @l2_feats = $self->feats_for_tnode($l2_tnode);
     }
     if (!defined $ali_info) {
         $ali_info = get_ali_info($l1_tnode, $l2_tnode);
     }
-    push @l2_feats, $ali_info;
+    push @l2_feats, ($ali_info // "No ali info");
 
     my @feats = (@l1_feats, @l2_feats);
 
@@ -124,19 +145,19 @@ sub process_tnode {
     print {$self->_file_handle} "\n";
 }
 
-sub process_anode {
-    my ($self, $l1_anode) = @_;
-    
-    my @l1_feats = feats_for_anode($l1_anode);
-    my ($l2_anode) = Treex::Tool::Align::Utils::aligned_transitively([$l1_anode], [$self->gold_align_filter]);
-    my @l2_feats = feats_for_anode($l2_anode);
-
-    my @feats = (@l1_feats, @l2_feats);
-    push @feats, get_ali_info($l1_anode, $l2_anode);
-
-    print {$self->_file_handle} (join "\t", @feats);
-    print {$self->_file_handle} "\n";
-}
+#sub process_anode {
+#    my ($self, $l1_anode) = @_;
+#    
+#    my @l1_feats = $self->feats_for_anode($l1_anode);
+#    my ($l2_anode) = Treex::Tool::Align::Utils::aligned_transitively([$l1_anode], [$self->gold_align_filter]);
+#    my @l2_feats = $self->feats_for_anode($l2_anode);
+#
+#    my @feats = (@l1_feats, @l2_feats);
+#    push @feats, get_ali_info($l1_anode, $l2_anode);
+#
+#    print {$self->_file_handle} (join "\t", @feats);
+#    print {$self->_file_handle} "\n";
+#}
 
 # TODO process_anode
 
